fa4bfb4344e0e3f747c97224d6097501744bdca4
Patrick510 <117788162+Patrick510@users.noreply.github.com>
2023-08-21 16:01:09 -0400

    commit-09

---
+++ trabalho/js/script.js
@@ -0,0 +1,17 @@
+class Produto {
+  constructor() {
+    this.id = 0;
+    this.nome = "";
+    this.celular = 0;
+  }
+
+  adicionar() {
+    alert("TESTE");
+  }
+
+  deletar() {}
+
+  editar() {}
+}
+
+var produto = new Produto();
