ee58f0944d4c8842d4df2df3cf165c663fb5331b
Patrick510 <117788162+Patrick510@users.noreply.github.com>
2023-08-22 21:00:08 -0400

    commit-20

--- trabalho/js/script.js
+++ trabalho/js/script.js
@@ -1,3 +1,4 @@
+//------------------------------------------------------------
 function inserirContato() {
   const contato = {
     nome: document.getElementById("nome").value,
@@ -7,8 +8,23 @@ function inserirContato() {
   const bd_contatos = getLocalStorage();
 
   bd_contatos.push(contato);
-}
 
+  //armazenar os contatos no LocalStorage
+  setLocalStorage(bd_contatos);
+
+  //atualizar apos a inserção
+  updateTable();
+}
+//---------------------STORAGE--------------------------------
 function getLocalStorage() {
   return JSON.parse(localStorage.getItem("bd_contatos"));
 }
+//------------------------------------------------------------
+function setLocalStorage(bd_contatos) {
+  localStorage.setItem("bd_contatos", JSON.stringify(bd_contatos));
+}
+
+//---------------------TABLE----------------------------------
+function updateTable() {}
+
+//------------------------------------------------------------
