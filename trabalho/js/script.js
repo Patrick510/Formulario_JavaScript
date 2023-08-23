2784e383eecdc63361d1d8f8d7bc2d45b98149c6
Patrick510 <117788162+Patrick510@users.noreply.github.com>
2023-08-23 17:24:40 -0400

    commit-29

--- trabalho/js/script.js
+++ trabalho/js/script.js
@@ -4,13 +4,26 @@ var arrayContatos = [];
 //------------------------ENVIO DOS DADOS------------------------------------
 function enviar() {
   let produto = this.lerDados();
+
+  this.listar();
+  this.clearval();
+
+  console.log(produto);
 }
 //---------------------LISTAGEM--------------------------------
 function listar() {}
 //----------------------ADICIONANDO OS DADOS----------------------------
 function adicionar(produto) {}
 //---------------------LENDO OS DADOS--------------------------
-function lerDados() {}
+function lerDados() {
+  let produto = {};
+
+  produto.id = this.id;
+  produto.nomeCont = document.getElementById("nome").value;
+  produto.fone = document.getElementById("fone").value;
+
+  return produto;
+}
 //----------------------VALIDAÇÃO DOS INPUTS-----------------------
 function validar() {}
 //-------------------------LIMPAR OS INPUTS-------------------------
