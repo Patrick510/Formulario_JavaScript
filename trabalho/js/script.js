48ff510539efa06add37391a590e733b7597da63
Patrick510 <117788162+Patrick510@users.noreply.github.com>
2023-08-22 20:32:19 -0400

    commit-18

--- trabalho/js/script.js
+++ trabalho/js/script.js
@@ -1,14 +1,6 @@
-function enviar() {
-  this.produto = this.cadContato();
-}
-
-function cadContato() {
-  this.id = 1;
-  this.arrayContatos = [];
-  this.produto = {};
-
-  produto.id = this.id;
-  produto.nomeProduto = document.getElementById("nome");
-  produto.celularProd = document.getElementById("celular");
-  return produto;
+function inserirContato() {
+  const contato = {
+    nome: document.getElementById("nome").value,
+    fone: document.getElementById("celular").value,
+  };
 }
