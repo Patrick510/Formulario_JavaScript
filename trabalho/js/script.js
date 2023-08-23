7ee3a8525399ad05bd94ebad9088c8abee4dc869
Patrick510 <117788162+Patrick510@users.noreply.github.com>
2023-08-22 20:46:25 -0400

    commit-19

--- trabalho/js/script.js
+++ trabalho/js/script.js
@@ -3,4 +3,12 @@ function inserirContato() {
     nome: document.getElementById("nome").value,
     fone: document.getElementById("celular").value,
   };
+
+  const bd_contatos = getLocalStorage();
+
+  bd_contatos.push(contato);
+}
+
+function getLocalStorage() {
+  return JSON.parse(localStorage.getItem("bd_contatos"));
 }
