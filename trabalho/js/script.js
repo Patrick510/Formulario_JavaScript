6674412c319d6d006eca36bc5ac2202991c9759b
Patrick510 <117788162+Patrick510@users.noreply.github.com>
2023-08-23 17:09:58 -0400

    commit-28

--- trabalho/js/script.js
+++ trabalho/js/script.js
@@ -1,54 +1,20 @@
+//----------------------------VARIAVEIS-------------------------------------
+var id = 1;
+var arrayContatos = [];
 //------------------------ENVIO DOS DADOS------------------------------------
-function inserirContato() {
-  const contato = {
-    nome: document.getElementById("nome").value,
-    fone: document.getElementById("fone").value,
-  };
-
-  const bd_contatos = getLocalStorage();
-  console.log(bd_contatos);
-
-  bd_contatos.push(contato);
-
-  //armazenar os contatos no LocalStorage
-  setLocalStorage(bd_contatos);
-
-  //atualizar apos a inserção
-  updateTable();
+function enviar() {
+  let produto = this.lerDados();
 }
-//---------------------STORAGE--------------------------------
-function getLocalStorage() {
-  return JSON.parse(localStorage.getItem("bd_contatos"));
-}
-//------------------------------------------------------------
-function setLocalStorage(bd_contatos) {
-  localStorage.setItem("bd_contatos", JSON.stringify(bd_contatos));
-}
-
-//---------------------TABLE----------------------------------
-function updateTable() {
-  //clean table
-
-  const bd_contatos = getLocalStorage();
-
-  bd_contatos.forEach(newRow);
-}
-
-//------------------------------------------------------------
-function newRow(contato, index) {
-  const line = document.createElement("tr");
-
-  line.innerHTML = ` 
-  <td>${index}</td>
-  <td>${contato.nome}</td>
-  <td>${contato.fone}</td>
-  <td>
-    <span class="material-symbols-outlined">
-      delete
-    </span>
-`;
-
-  document.querySelector("#bdContato>tbody").appendChild(line);
-}
-//------------------------------------------------------------
-function cleanTable() {}
+//---------------------LISTAGEM--------------------------------
+function listar() {}
+//----------------------ADICIONANDO OS DADOS----------------------------
+function adicionar(produto) {}
+//---------------------LENDO OS DADOS--------------------------
+function lerDados() {}
+//----------------------VALIDAÇÃO DOS INPUTS-----------------------
+function validar() {}
+//-------------------------LIMPAR OS INPUTS-------------------------
+function clearval() {}
+//------------------DELETA A LINHA DA TABELA-------------------------
+function deletar(id) {}
+//-------------------------------------------------------------------
